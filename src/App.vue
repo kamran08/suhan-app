1d494b2049a17790e1d6f4ac37e50c66b4ed725f
kamran08 <ahmedkamran265@gmail.com>
2023-03-16 16:01:36 +0000

    'updated'

---
+++ src/App.vue
@@ -0,0 +1,129 @@
+ <template>
+  <div id="app">
+    <div id="main-wrapper">
+      <!-- Header -->
+      <!-- <div class="_1header">
+        <div class="container-fuild">
+          <div class="row align-items-center">
+            <div class="col-12 col-md-4 col-lg-4">
+              <p class="_1header_text">
+                HEAD OFFICE
+                <router-link to="">1760 Wilshire Blvd., LA, CA 90025</router-link>
+              </p>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+              <p class="_1header_text">
+                MAIL
+                <router-link to="">example@example.com</router-link>
+              </p>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+              <p class="_1header_text _last_child">
+                HEAD OFFICE
+                <router-link to="">+1 310 500 7834</router-link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div> -->
+      <!-- Header -->
+
+      <!-- Menu -->
+      <div class="_1menu">
+        <div class="container-fuild">
+          <div class="row align-items-center">
+            <div class="col-12 col-md-auto col-lg-auto">
+              <router-link to="" class="_1menu_logo">
+                <!-- <h2 style="color:var(--color2)"> -->
+                  <img class="_1menu_logo_img" src="../static/img/logo.png" alt="" title="">
+                <!-- </h2> -->
+                
+              </router-link>
+            </div>
+
+            <div class="col-12 col-md col-lg">
+              <ul class="_1menu_logo_list">
+                <li>
+                  <a href="#" class="_1menu_logo_list_text _active">Home</a>
+                </li>
+                <li>
+                  <a href="#1" class="_1menu_logo_list_text">Services</a>
+                </li>
+                <li>
+                  <a href="#2" class="_1menu_logo_list_text">Contact us</a>
+                </li>
+              </ul>
+            </div>
+
+            <!-- <div class="col-12 col-md-auto col-lg-auto">
+              <ul class="_1menu_social">
+                <li>
+                  <a href=""><Icon type="logo-facebook" /></a>
+                </li>
+                <li>
+                  <a href=""><Icon type="logo-linkedin" /></a>
+                </li>
+                <li>
+                  <a href=""><Icon type="logo-twitter" /></a>
+                </li>
+                <li>
+                  <a href=""><Icon type="logo-youtube" /></a>
+                </li>
+              </ul>
+            </div> -->
+          </div>
+        </div>
+      </div>
+      <!-- Menu -->
+
+      <div class="_main_layout">
+        <router-view></router-view>
+      </div>
+
+      <!-- Footer -->
+      <div v-if="$route.path != '/messenger'" class="_log_footer">
+        <ul class="_log_footer_list">
+          <li>
+              <router-link to="/about">About</router-link>
+          </li>
+          <li>
+              <router-link to="/support">Support</router-link>
+          </li>
+          <li>
+              <router-link to="/donate">Donate</router-link>
+          </li>
+          <li>
+              <router-link to="/terms">Terms</router-link>
+          </li>
+          <li>
+              <router-link to="/privacy">Privacy</router-link>
+          </li>
+        </ul>
+
+        <p class="_log_footer_copy">
+            Services©2023. developed by 
+            <a href="">ahmedkamran265@gmail.com</a>
+        </p>
+      </div>
+      <!-- Footer -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+     
+    }
+  },
+
+  methods:{
+
+  },
+  
+  created() {
+    
+  }
+}
+</script>
---
+++ src/components/index.vue
@@ -0,0 +1,184 @@
+<template>
+    <div>
+        
+        <div class="_1main_content">
+            <!-- Banner -->
+            <div class="_1banner">
+                <div class="container">
+                    <div class="col-12 col-md-8 col-lg-6">
+                        <!-- <h1 class="_1banner_title">LEGAL CASES</h1> -->
+                        <h2 class="_1banner_subtitle">SUHAN & GIAS with a wide range of clients and maintain strong relationships with them. Our key aim is to provide best services like ,</h2>
+                        <p class="_1banner_text">
+                            Carpentry & wood flooring works, Painting Contracting, Air-conditioning, ventilation & Air Filtration Systems installation & Maintenace, Electromechanical Equipment Installation and Maintenace
+                            ,Wallpaper Fixing works, Electrical Fittings & Fixtures Repairing & maintenance, Building Cleaning Services, Floor & Wall Tiling Works, Plumbing & sanitary Installation, Plaster Works, Engraving & Ornamentation Works,  False Ceiling & Light Partitions Installation.
+                            </p>
+                        <!-- <button class="_2btn _btn_large">Find Out More</button> -->
+                    </div>
+                </div>
+            </div>
+            <!-- Banner -->
+
+            <!-- Services -->
+            <div class="_1home_services" id="1">
+                <div class="container">
+                    <h1 class="_home_title">Our services</h1>
+
+                    <div class="row">
+                        <!-- Card -->
+                        <div class="col-12 col-md-4 col-lg-4">
+                            <router-link to="" class="_1card">
+                                <div class="_1card_img">
+                                    <img class="_1card_pic" src="../../static/img/woodwork.png" alt="" title="">
+                                </div>
+
+                                <div class="_1card_bottom">
+                                    <p class="_1card_title">Carpentry & wood flooring works. </p>
+                                    <!-- <p class="_1card_text"> Carpentry & wood flooring works</p> -->
+                                </div>
+                            </router-link>
+                        </div>
+                        <!-- Card -->
+                        <!-- Card -->
+                        <div class="col-12 col-md-4 col-lg-4">
+                            <router-link to="" class="_1card">
+                                <div class="_1card_img">
+                                    <img class="_1card_pic" src="../../static/img/painting.png" alt="" title="">
+                                </div>
+
+                                <div class="_1card_bottom">
+                                    <p class="_1card_title">Painting Contracting.</p>
+                                    <!-- <p class="_1card_text">Litigation Issues Arising from Transactions</p> -->
+                                </div>
+                            </router-link>
+                        </div>
+                        <!-- Card -->
+                        <!-- Card -->
+                        <div class="col-12 col-md-4 col-lg-4">
+                            <router-link to="" class="_1card">
+                                <div class="_1card_img">
+                                    <img class="_1card_pic" src="../../static/img/air-conditioning.png" alt="" title="">
+                                </div>
+
+                                <div class="_1card_bottom">
+                                    <p class="_1card_title">Air-conditioning</p>
+                                    <p class="_1card_text">Air-conditioning, ventilation & Air Filtration Systems installation & Maintenace</p>
+                                </div>
+                            </router-link>
+                        </div>
+                        <!-- Card -->
+                        <!-- Card -->
+                        <div class="col-12 col-md-4 col-lg-4">
+                            <router-link to="" class="_1card">
+                                <div class="_1card_img">
+                                    <img class="_1card_pic" src="../../static/img/Electromechanical.png" alt="" title="">
+                                </div>
+
+                                <div class="_1card_bottom">
+                                    <p class="_1card_title">Electromechanical</p>
+                                    <p class="_1card_text">Electromechanical Equipment Installation and Maintenace</p>
+                                </div>
+                            </router-link>
+                        </div>
+                        <!-- Card -->
+                        <!-- Card -->
+                        <div class="col-12 col-md-4 col-lg-4">
+                            <router-link to="" class="_1card">
+                                <div class="_1card_img">
+                                    <img class="_1card_pic" src="../../static/img/wallpaper.png" alt="" title="">
+                                </div>
+
+                                <div class="_1card_bottom">
+                                    <p class="_1card_title">Wallpaper Fixing works</p>
+                                    <!-- <p class="_1card_text"> Not Confer Litigation Issues Arising from Transactions</p> -->
+                                </div>
+                            </router-link>
+                        </div>
+                        <!-- Card -->
+                        <!-- Card -->
+                        <div class="col-12 col-md-4 col-lg-4">
+                            <router-link to="" class="_1card">
+                                <div class="_1card_img">
+                                    <img class="_1card_pic" src="../../static/img/cleaning.png" alt="" title="">
+                                </div>
+
+                                <div class="_1card_bottom">
+                                    <p class="_1card_title">Building Cleaning Services</p>
+                                    <!-- <p class="_1card_text"> Not Confer Litigation Issues Arising from Transactions</p> -->
+                                </div>
+                            </router-link>
+                        </div>
+                        <!-- Card -->
+                    </div>
+                </div>
+            </div>
+            <!-- Services -->
+
+            <!-- Contact -->
+            <div class="_home_contact" id="2">
+                <div class="_home_contact_left">
+                    <h1 class="_home_title">Free Case Evaluation</h1>
+                    <p class="_home_contact_text">Contacting the firm is free. We understand that the disputes facing you, your family or your business can seem daunting.</p>
+
+                    <div class="_home_contact_sub">
+                        <div class="_home_contact_input">
+                            <input placeholder="Name" class="_home_contact_int">
+                        </div>
+                        <div class="_home_contact_input">
+                            <input placeholder="Email" class="_home_contact_int">
+                        </div>
+                        <div class="_home_contact_input">
+                            <input placeholder="Subject" class="_home_contact_int">
+                        </div>
+                        <div class="_home_contact_input">
+                            <textarea :rows="4" class="_home_contact_int" placeholder="Message"></textarea>
+                        </div>
+
+                        <div class="_home_contact_button">
+                            <button class="_1btn _btn_large">Submit</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="_home_contact_right">
+                    <h1 class="_home_title">Contact us</h1>
+
+                    <p class="_home_contact_text">With our recognized leadership in applying legal service and technology innovation, we deliver commercial advantage to clients.</p>
+
+                    <div class="_home_contact_main">
+                        <p class="_home_contact_info">
+                            OFFICE
+                            <span class="_home_contact_info_text">7 Naif Building 10 Nkheel st. 
+                                Near Sonar Bangla Grocery, Dubai -U.A.E</span>
+                        </p>
+                        <p class="_home_contact_info">
+                            EMAIL
+                            <router-link to="" class="_home_contact_info_text">suhanchowdhury1997@gmail.com </router-link>
+                        </p>
+                        <p class="_home_contact_info">
+                            Mobile
+                            <router-link to="" class="_home_contact_info_text"> +9710562855140</router-link>
+                        </p>
+                    </div>
+
+                    <div class="_home_contact_socail">
+                        <ul class="_1menu_social">
+                            <li>
+                                <a href=""><Icon type="logo-facebook" /></a>
+                            </li>
+                            <li>
+                                <a href=""><Icon type="logo-linkedin" /></a>
+                            </li>
+                            <li>
+                                <a href=""><Icon type="logo-twitter" /></a>
+                            </li>
+                            <li>
+                                <a href=""><Icon type="logo-youtube" /></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- Contact -->
+        </div>
+    </div>
+</template>
